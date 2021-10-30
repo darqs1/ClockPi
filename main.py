a2470e0b6e66e98524414aa4b95bc606fce4d294
DarQs <darqs1@gmail.com>
2021-10-30 18:16:23 +0200

    0.5 test 1

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from libs.matrixled import MatrixLed
 from libs.variables import version
 from time import sleep
+from libs.messages import Messages
 
 mled = MatrixLed()
 mled.showMessage("ClockPi v." + version, 0.01)
+mes = Messages()
 
 try:
     mled.clockStart()
+    while True:
+        if (mes.messages.count > 0):
+            print('There is message to show')
+        else:
+            print('zero messages to print')
+        sleep(1)
+
 except KeyboardInterrupt:
     mled.showMessage('Goodbye', 0.01)
     sleep(1)
