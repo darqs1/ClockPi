e4294942d0bd66a7413befbb868fdb36e6b8febc
DarQs <darqs1@gmail.com>
2021-10-30 18:01:22 +0200

    [ADD] Sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from libs.matrixled import MatrixLed
 from libs.variables import version
+from time import sleep
 
 mled = MatrixLed()
 mled.showMessage("ClockPi v." + version, 0.01)
@@ -8,4 +9,5 @@ try:
     mled.clockStart()
 except KeyboardInterrupt:
     mled.showMessage('Goodbye', 0.01)
+    sleep(1)
     mled.clockStop()
