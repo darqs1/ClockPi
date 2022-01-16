3e63ca14b8771b76a32be75e7cd4a67cc849c210
DarQs <darqs1@gmail.com>
2022-01-16 13:54:08 +0100

    [test]

--- libs/messages.py
+++ libs/messages.py
@@ -8,7 +8,7 @@ class Messages:
         with open(os.getcwd() + '/messages.txt') as f:
             lines = f.readlines()
             for line in lines:
-                if (line!='') and (line!='\\n'):
+                if (line!='') and (line!='\n'):
                     self.messages.append(line)
             print("Messages:", self.messages)
         
