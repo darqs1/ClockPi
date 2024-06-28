3361b79689341300d1c87330996f60a715a6d56f
DarQs <darqs1@gmail.com>
2024-06-28 12:34:44 +0200

    v1..06

--- libs/matrixled.py
+++ libs/matrixled.py
@@ -23,17 +23,50 @@ class MatrixLed:
         show_message(self.device, message, fill="white", font=proportional(LCD_FONT), scroll_delay=delay)
 
     def clockLoop(self):
+        step = 10
+        step_now = 1
+
         point = True
+        move_right = True
+        margin = 0
+        second = datetime.now().strftime('%S')
         while True:
             with canvas(self.virtual) as draw:
-                point = not point
-                text(draw, (0, 1), datetime.now().strftime('%H'), fill="white", font=proportional(LCD_FONT))
-                text(draw, (12, 1), ':' if point else ' ', fill="white", font=proportional(TINY_FONT))
-                text(draw, (14, 1), datetime.now().strftime('%M'), fill="white", font=proportional(LCD_FONT))
-                text(draw, (26, 1), ' ' if point else ':', fill="white", font=proportional(TINY_FONT))
-                text(draw, (28, 1), datetime.now().strftime('%S'), fill="white", font=proportional(LCD_FONT))
-                text(draw, (46, 1), datetime.now().strftime('%d.%m'), fill="white", font=proportional(TINY_FONT))
-                sleep(1)
+                second_now = datetime.now().strftime('%S')
+                if second!=second_now:
+                    point = not point
+                    second=second_now
+
+                text(draw, (0+margin, 1 if move_right else 0), datetime.now().strftime('%H'), fill="white", font=proportional(LCD_FONT))
+                text(draw, (12+margin, 1 if move_right else 0), ':' if point else ' ', fill="white", font=proportional(TINY_FONT))
+                text(draw, (14+margin, 1 if move_right else 0), datetime.now().strftime('%M'), fill="white", font=proportional(LCD_FONT))
+                text(draw, (26+margin, 1 if move_right else 0), ' ' if point else ':', fill="white", font=proportional(TINY_FONT))
+                text(draw, (28+margin, 1 if move_right else 0), datetime.now().strftime('%S'), fill="white", font=proportional(LCD_FONT))
+                if margin < 8:
+                    text(draw, (47, 1 if move_right else 0), datetime.now().strftime('%d.%m'), fill="white", font=proportional(TINY_FONT))
+                if margin > 17:
+                    text(draw, (0, 1 if move_right else 0), datetime.now().strftime('%d.%m'), fill="white", font=proportional(TINY_FONT))
+
+                if margin >=8 and margin <=17:
+                    text(draw, (0, 1 if move_right else 0), datetime.now().strftime('%d'), fill="white", font=proportional(TINY_FONT))
+                    text(draw, (57, 1 if move_right else 0), datetime.now().strftime('%m'), fill="white", font=proportional(TINY_FONT))
+
+                if step_now == step:
+                    step_now = 1
+
+                    if move_right:
+                        margin += 1
+                    else:
+                        margin -= 1
+
+                    if margin > 24 or margin == 0:
+                        move_right = not move_right
+                else:
+                    step_now += 1
+
+
+                sleep(0.1)
+                # self.showText(margin)
 
     def showText(self, showText):
         with canvas(self.virtual) as draw:
